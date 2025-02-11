55ed65d393db3966f10831e0e4f7922a8e132e50
Muhammad Fadhlan Karimuddin <138226358+poemich@users.noreply.github.com>
2025-02-11 20:53:28 +0700

    add ProductService and ProductServiceImpl

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/service/ProductService.java
@@ -0,0 +1,10 @@
+package id.ac.ui.cs.advprog.eshop.service;
+
+import java.util.List;
+
+import id.ac.ui.cs.advprog.eshop.model.Product;
+
+public interface ProductService {
+    public Product create(Product product);
+    public List<Product> findAll();
+}
