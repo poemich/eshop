dec5c5ce6dd49c9ad1f167c6d31c57ed5e96a9b6
Muhammad Fadhlan Karimuddin <fadhlankarimuddin@gmail.com>
2025-02-14 15:45:02 +0700

    dependencies fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,11 @@ repositories {
     mavenCentral()
 }
 
+val seleniumJavaVersion = "4.14.1"
+val seleniumJupiterVersion = "5.0.1"
+val webdriverManagerVersion = "5.6.3"
+val junitJupiterVersion = "5.9.1"
+
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
@@ -32,15 +37,15 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-    testImplementation("org.seleniumhg. selenium:selenium-java : $seleniumJavaVersion")
-    testImplementation("io. github. bonigarcia:selenium-jupiter : $seleniumJupiterVersion")
-    testImplementation("io. github. bonigarcia:webdrivermanager:$webdrivermanagerVersion")
-    testImplementation("org. junit. jupiter: junit-jupiter-api: $junitJupiterVersion")
-    testRuntimeOnly("org. junit. jupiter: junit-jupiter-engine:$junitJupiterVersion")
-}
+    testImplementation("org.seleniumhq.selenium:selenium-java:$seleniumJavaVersion")
+    testImplementation("io.github.bonigarcia:selenium-jupiter:$seleniumJupiterVersion")
+    testImplementation("io.github.bonigarcia:webdrivermanager:$webdriverManagerVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
 
-tasks.register<Test>("unitTest") {
-    description = "Run unit tests."
+}
+tasks.register<Test>(name = "unitTest") {
+    description = "Runs unit tests."
     group = "verification"
 
     filter {
@@ -48,8 +53,8 @@ tasks.register<Test>("unitTest") {
     }
 }
 
-tasks.register<Test>("functionalTest") {
-    description = "Run functional tests."
+tasks.register<Test>(name = "functionalTest") {
+    description = "Runs functional tests."
     group = "verification"
 
     filter {
@@ -60,8 +65,3 @@ tasks.register<Test>("functionalTest") {
 tasks.withType<Test>().configureEach {
     useJUnitPlatform()
 }
-
-val seleniumJavaVersion = "4.14.1"
-val seleniumJupiterVersion = "5.0.1"
-val webdrivermanagerVersion = "5.6.3"
-val junitJupiterVersion = "5.9.1"
