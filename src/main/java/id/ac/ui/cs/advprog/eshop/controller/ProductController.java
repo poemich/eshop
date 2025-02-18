bdbb25d94af3293181ea0964890d05182ce2d3f8
Muhammad Fadhlan Karimuddin <fadhlankarimuddin@gmail.com>
2025-02-18 19:09:14 +0700

    [Code Quality] Remove this field injection and use constructor injection instead.

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -18,8 +18,12 @@ import id.ac.ui.cs.advprog.eshop.service.ProductService;
 @RequestMapping("/product")
 public class ProductController {
 
+    private final ProductService service;
+
     @Autowired
-    private ProductService service;
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
 
     @GetMapping("/create")
     public String createProductPage(Model model) {
