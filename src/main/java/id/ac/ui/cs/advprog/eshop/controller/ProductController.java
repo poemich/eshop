41a4fabd533c5c2539469d47070b125a4fee8073
Muhammad Fadhlan Karimuddin <fadhlankarimuddin@gmail.com>
2025-02-27 10:18:44 +0700

    add profile annotations for CarController and ProductControllerTest

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -3,6 +3,7 @@ package id.ac.ui.cs.advprog.eshop.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -73,6 +74,7 @@ public class ProductController {
     }
 }
 
+@Profile("!test")
 @Controller
 @RequestMapping("/car")
 class CarController extends ProductController {
--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTest.java
@@ -13,11 +13,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import id.ac.ui.cs.advprog.eshop.model.Product;
 import id.ac.ui.cs.advprog.eshop.service.ProductService;
 
+@ActiveProfiles("test")
 @WebMvcTest(ProductController.class)
 class ProductControllerTest {
 
